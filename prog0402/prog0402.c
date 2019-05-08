1435016ed50960d328bef403304439adab928c67
2tf <39296516+nitf@users.noreply.github.com>
2019-05-08 14:48:40 +0900

    [add] prog0402

---
+++ prog0402/prog0402.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+	// printf("a\nab\nabc\nabcd\n");
+	
+	// ‰Â“Ç«‚ğd‹‚·‚é‚È‚ç
+	
+	printf("a\n");
+	printf("ab\n");
+	printf("abc\n");
+	printf("abcd\n");
+	
+	return 0;
+}
