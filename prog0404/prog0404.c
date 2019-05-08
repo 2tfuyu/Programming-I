96c60c246def6aee0c3aa1b2594035f4cbb3968d
2tf <39296516+nitf@users.noreply.github.com>
2019-05-08 14:49:22 +0900

    [add] prog0404

---
+++ prog0404/prog0404.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("CŒ¾Œê‚ÌƒvƒƒOƒ‰ƒ€‚Å\n");
+	printf("	printf‚Å•¶š‚ğ•\¦‚µ‚Ä\n");
+	printf("		‚È‚ñ‚Ì–ğ‚É—§‚Â‚Ì‚Å‚µ‚å‚¤\n");
+	printf("‹³‚¦‚Ä‚­‚¾‚³‚¢\n");
+	
+	return 0;
+}
