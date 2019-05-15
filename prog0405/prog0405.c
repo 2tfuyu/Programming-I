f20ea1d6b34b3d8aa1ca493e9a2239964c562134
2tf <39296516+nitf@users.noreply.github.com>
2019-05-15 10:13:46 +0900

    [add] prog0405

---
+++ prog0405/prog0405.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("®”%d‚ğ•\¦‚µ‚Ü‚µ‚½\n", 10);
+	printf("10‚É5‚ğ‰Á‚¦‚é‚Æ%d‚Æ‚È‚è‚Ü‚·\n", 10 + 5);
+	printf("%d*%d=%d\n", 2, 3, 2 * 3);
+	
+	return (0);
+}
