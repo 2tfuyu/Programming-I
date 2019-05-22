82f822c4b08002fababe2df240477b803cf1c80e
Takaki <2tf2003@gmail.com>
2019-05-22 10:17:44 +0900

    [add] prog0506

---
+++ prog0506/prog0506.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+	float teihen = 2.3;
+	float takasa = 1.5;
+	float menseki = (teihen * takasa) / 2;
+	
+	printf("’ê•Ó%f‚Å‚‚³‚Í%f‚ÌOŠpŒ`‚Ì–ÊÏ‚Í%f‚Å‚·\n", teihen, takasa, menseki);
+	
+	return (0);
+}
