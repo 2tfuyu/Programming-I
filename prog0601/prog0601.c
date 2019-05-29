a38ce16b37489f8beae27194d8c894bf0ff06c08
Takaki <2tf2003@gmail.com>
2019-05-29 10:03:03 +0900

    [add] prog0601

---
+++ prog0601/prog0601.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+// #include <math.h>
+
+int main(void)
+{
+	int a = 5;
+	int aaa = a * a * a;
+	// int aaa = pow(a, 3.0);
+	
+	printf("%d‚Ì3æ‚Í%d‚Å‚·\n", a, aaa);
+	return (0);
+}
