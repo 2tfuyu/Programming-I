60c3d45c84ec270198f453c4d697e7593f0602cd
Takaki <2tf2003@gmail.com>
2019-05-29 10:03:24 +0900

    [add] prog0602

---
+++ prog0602/prog0602.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+	float pi;
+	float r;
+	float s;
+	
+	pi = 3.1416;
+	r = 10.0;
+	s = r * r * pi;
+	
+	printf("”¼Œa%.0f‚Ì‰~‚Ì–ÊÏ‚Í%.0f‚Å‚·B\n", r, s);
+
+	return (0);
+}
