f34a9ab2e8e79e45073486e050eed1d31344e079
Takaki <2tf2003@gmail.com>
2019-07-03 10:15:22 +0900

    [add] prog1002

---
+++ prog1002/prog1002.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+	float a;
+	float b;
+	
+	printf("a=");
+	scanf("%f", &a);
+	
+	printf("b=");
+	scanf("%f", &b);
+	
+	printf("a+b=%f\n", a + b);
+	
+	return (0);
+}
