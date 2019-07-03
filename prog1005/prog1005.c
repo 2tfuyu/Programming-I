dfd689922f683ad4a8d622644d27e4c6170c9326
Takaki <2tf2003@gmail.com>
2019-07-03 10:16:10 +0900

    [add] prog1005

---
+++ prog1005/prog1005.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int a;
+	
+	printf("”’l(3Œ…)=");
+	scanf("%d", &a);
+	
+	printf("1‚ÌˆÊ%d\n",a%10);
+	printf("10‚ÌˆÊ%d\n",(a/10)%10);
+	printf("100‚ÌˆÊ%d\n",a/100);
+	
+	return (0);
+}
