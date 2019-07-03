6d018ac07e3cdca90de85406d0c68b1b09f2100e
Takaki <2tf2003@gmail.com>
2019-07-03 10:15:56 +0900

    [add] prog1004

---
+++ prog1004/prog1004.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int a;
+	int b;
+	int c;
+	
+	printf("”’l1=");
+	scanf("%d", &a);
+	
+	printf("”’l2=");
+	scanf("%d", &b);
+	
+	printf("”’l3=");
+	scanf("%d", &c);
+	
+	printf("•½‹Ï=%f", (a + b + c) / 3.0);
+	
+	return (0);
+}
