1911a801131e8e134d4cc80f9bbe974220640276
Takaki <2tf2003@gmail.com>
2019-07-10 10:18:01 +0900

    [add] prog1102

---
+++ prog1102/prog1102.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int i;
+	
+	printf("®”‚ğ“ü—Í‚µ‚Ä‚­‚¾‚³‚¢: ");
+	scanf("%d", &i);
+	
+	if (i == 0) {
+		printf("%d‚Í0‚Å‚·\n");
+	}
+	else {
+		printf("%d‚Í0‚Å‚Í‚ ‚è‚Ü‚¹‚ñ\n");
+	}
+	
+	return (0);
+}
