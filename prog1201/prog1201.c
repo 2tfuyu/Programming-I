0b0b135785774cbfc330e840659e66997d89f307
2tf <2tf2003@gmail.com>
2019-07-17 10:27:41 +0900

    [add] prog1201

---
+++ prog1201/prog1201.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int i;
+	scanf("%d", &i);
+	
+	if (i % 3 == 0) {
+		printf("%d‚Í3‚Ì”{”‚Å‚·B\n", i);
+	}
+	else {
+		printf("%d‚Í3‚Ì”{”‚Å‚Í‚ ‚è‚Ü‚¹‚ñB\n", i);
+	}
+	
+	return (0);
+}
