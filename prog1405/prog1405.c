f517b2d9652f7ffdfaf3fe223b364c74c0febacb
Takaki Hamahiga <2tf2003@gmail.com>
2019-09-27 10:02:28 +0900

    [add] prog1405

---
+++ prog1405/prog1405.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int n;
+	
+	printf("”Ô†=");
+	scanf("%d", &n);
+	
+	if ((n == 2) || (n == 4) || (n == 5) || (n == 7) || (n == 8) || (n == 9) || (n == 10)) {
+		printf("’jq‚Å‚·\n");
+	}
+	else {
+		printf("—q‚Å‚·\n");
+	}
+	return (0);
+}
