9766cdf53b05c1f60567b4bdb87f276900714c24
Takaki Hamahiga <2tf2003@gmail.com>
2019-09-27 10:03:07 +0900

    [add] prog1407

---
+++ prog1407/prog1407.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int i;
+	
+	printf("Ž^¬ŽÒ”=");
+	scanf("%d", &i);
+	
+	if ((i >= 21) && (i <= 41)) {
+		printf("‘½”Œˆ‚Å‰ÂŒˆ‚µ‚Ü‚µ‚½B\n");
+	}
+	else {
+		printf("”ÛŒˆ‚µ‚Ü‚µ‚½\n");
+	}
+	return (0);
+}
