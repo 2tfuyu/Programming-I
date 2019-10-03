88ffc0312b0732896f01fd28654068ef2864dc8a
Takaki Hamahiga <2tf2003@gmail.com>
2019-10-04 08:57:03 +0900

    add: prog1710

---
+++ prog1710/prog1710.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int i;
+	int ans = 0;
+	
+    for (i = 1; i <= 10; i++) {
+		ans += i;
+    }
+	printf("1‚©‚ç10‚Ü‚Å‚Ì‡Œv‚Í%d‚Å‚·\n", ans);
+    return 0;
+}
