a28cf6a30141d5157b696373b882cbd57176e99b
Takaki Hamahiga <2tf2003@gmail.com>
2019-10-04 10:28:18 +0900

    add: prog1806

---
+++ prog1806/prog1806.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int x, y;
+	int ans = 1;
+	
+	while (ans % 5 != 0) {
+		printf("®”1=");
+		scanf("%d", &x);
+	
+		printf("®”2=");
+		scanf("%d", &y);
+	
+		ans = x + y;
+	}
+	return (0);
+}
