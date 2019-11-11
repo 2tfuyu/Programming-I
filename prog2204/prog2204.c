04f356a31bab2c5db3a7cf5e7b17b4b051e8516e
Takaki Hamahiga <2tf2003@gmail.com>
2019-11-11 11:44:20 +0900

    add: prog2204

---
+++ prog2204/prog2204.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int i;
+	int sum;
+	
+	sum = 0;
+	for (i = 1; i <= 10; i++) {
+		sum += 1;
+	}
+	printf("1‚©‚ç10‚Ü‚Å‚Ì‡Œv‚Í%d‚Å‚·\n", sum);
+	return (0);
+}
