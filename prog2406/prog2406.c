95a408df1bb885820e5c2fe48e1e13bcb5356558
Takaki Hamahiga <2tf2003@gmail.com>
2019-11-11 11:50:58 +0900

    add: prog2406

---
+++ prog2406/prog2406.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int x;
+	printf("®”=");
+	scanf("%d", &x);
+	
+	if (x % 2 == 0) {
+		printf("‹ô”‚Å‚·\n");
+	}
+	else {
+		printf("Šï”‚Å‚·\n");
+	}
+	return (0);
+}
