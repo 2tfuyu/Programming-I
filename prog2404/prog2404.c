67554ab4cd18608b761a87fb9967ae44d9d5871a
Takaki Hamahiga <2tf2003@gmail.com>
2019-11-11 11:50:21 +0900

    add: prog2404

---
+++ prog2404/prog2404.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int x;
+	int i, j;
+	
+	printf("x=");
+	scanf("%d", &x);
+	
+	for (i = 1; i <= x; i++) {
+		for (j = 1; j <= x; j++) {
+			printf("%d + %d = %d\n", i, j, i + j);
+		}
+	}
+	return (0);
+}
